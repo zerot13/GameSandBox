e74d13780e7f71a3db63d64aaf912e930204878a
Ian <imac0.13@gmail.com>
2017-09-09 01:51:55 -0700

    Updates to GameWindow

--- GameSandBox/Game.cs
+++ GameSandBox/Game.cs
@@ -23,6 +23,38 @@ namespace GameSandBox
             _window.Load += Window_Load;
             _window.UpdateFrame += Window_UpdateFrame;
             _window.RenderFrame += Window_RenderFrame;
+            _window.KeyDown += Window_KeyDown;
+            _window.KeyUp += Window_KeyUp;
+            _window.KeyPress += Window_KeyPress;
+            _window.MouseMove += Window_MouseMove;
+            _window.MouseWheel += Window_MouseWheel;
+        }
+
+        private void Window_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            Console.WriteLine("Delta: {0} / {1}\nValue: {2} / {3}\n", e.Delta, e.DeltaPrecise, e.Value, e.ValuePrecise);
+        }
+
+        Vector2 mousePos;
+        private void Window_MouseMove(object sender, MouseMoveEventArgs e)
+        {
+            //Console.WriteLine("MouseMove: {0}, {1}", e.X, e.Y);
+            mousePos = new Vector2(e.X, e.Y);
+        }
+
+        private void Window_KeyDown(object sender, KeyboardKeyEventArgs e)
+        {
+            //Console.WriteLine("KeyDown: {0} - {1}", e.Key, sender);
+        }
+
+        private void Window_KeyUp(object sender, KeyboardKeyEventArgs e)
+        {
+            //Console.WriteLine("KeyUp: {0} - {1}", e.Key, sender);
+        }
+
+        private void Window_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //Console.WriteLine("KeyPress: {0} - {1}", e.KeyChar, sender);
         }
 
         private void Window_Load(object sender, EventArgs e)
@@ -40,10 +72,21 @@ namespace GameSandBox
             texture = ContentPipe.LoadTexture("Content/penguin.png");
         }
 
+        KeyboardState prevState;
         private void Window_UpdateFrame(object sender, FrameEventArgs e)
         {
-            //Console.WriteLine("Update");
-            //throw new NotImplementedException();
+            KeyboardState keyState = Keyboard.GetState();
+
+            if (keyState.IsKeyDown(Key.Enter) && prevState.IsKeyUp(Key.Enter))
+            {
+                Console.WriteLine("Enter!");
+            }
+            prevState = keyState;
+
+            //Console.WriteLine("MousePos: {0}, {1}", _window.Mouse.X, _window.Mouse.Y);
+            MouseState mState = Mouse.GetCursorState();
+            Point mPos = _window.PointToClient(new Point(mState.X, mState.Y));
+            //Console.WriteLine("MousePos: {0}, {1}", mPos.X, mPos.Y);
         }
 
         private void Window_RenderFrame(object sender, FrameEventArgs e)
