7f6788569d3d8bb1a28ba7b25eab2e9f9e457f28
Ian <imac0.13@gmail.com>
2017-09-22 03:09:45 -0700

    First draft of win conditions

--- GameSandBox/GameState.cs
+++ GameSandBox/GameState.cs
@@ -46,12 +46,83 @@ namespace GameSandBox
         {
             int index = hQuads * y + x;
             gameBoard[index] = curPlayer + 1;
-            return CheckWin();
+            return CheckWin(x, y, curPlayer + 1);
         }
 
-        private bool CheckWin()
+        private bool CheckWin(int x, int y, int curPlayer)
         {
-            return false;
+            bool isWin = true;
+            // Check vertical win condition
+            for (int yIndex = 0; yIndex < vQuads; yIndex++)
+            {
+                int test = (yIndex * hQuads) + x;
+                int test2 = gameBoard[(yIndex * hQuads) + x];
+                if(gameBoard[(yIndex * hQuads) + x] != curPlayer)
+                {
+                    isWin = false;
+                    break;
+                }
+            }
+            if (isWin)
+            {
+                return true;
+            }
+            isWin = true;
+            // Then, check horizontal win condition
+            for (int xIndex = 0; xIndex < hQuads; xIndex++)
+            {
+                int test = (y * hQuads) + xIndex;
+                int test2 = gameBoard[(y * hQuads) + xIndex];
+                if (gameBoard[(y * hQuads) + xIndex] != curPlayer)
+                {
+                    isWin = false;
+                    break;
+                }
+            }
+            if (isWin)
+            {
+                return true;
+            }
+            isWin = true;
+            // Finally, check diagonal win condition (if possible)
+            if(hQuads == vQuads)
+            {
+                // If x == y, the top left to bottom right diag is possible
+                if(x == y)
+                {
+                    for(int index = 0; index < hQuads; index++)
+                    {
+                        if(gameBoard[index * (hQuads + 1)] == curPlayer)
+                        {
+                            isWin = false;
+                            break;
+                        }
+                    }
+                    if (isWin)
+                    {
+                        return true;
+                    }
+                    isWin = true;
+                }
+                
+                // if x + y + 1 == hQuads (or vQuads), the bottom left to top right diag is possible
+                if(x + y + 1 == hQuads)
+                {
+                    for (int index = 1; index <= hQuads; index++)
+                    {
+                        if (gameBoard[index * (hQuads - 1)] == curPlayer)
+                        {
+                            isWin = false;
+                            break;
+                        }
+                    }
+                    if (isWin)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return isWin;
         }
     }
 }
