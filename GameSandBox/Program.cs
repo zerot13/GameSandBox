0c04b9aeef16e7ac30e6734b405f4510a31dabeb
Ian Machuca <imac0.13@gmail.com>
2021-10-29 16:15:45 -0700

    Reverting to original basewindow

--- GameSandBox/Program.cs
+++ GameSandBox/Program.cs
@@ -10,8 +10,7 @@ namespace GameSandBox
     {
         static void Main(string[] args)
         {
-            // OpenTK.GameWindow baseWindow = new OpenTK.GameWindow(800, 600, new OpenTK.Graphics.GraphicsMode(32, 8, 0, 0));
-            OpenTK.GameWindow baseWindow = new OpenTK.GameWindow(800, 600);
+            OpenTK.GameWindow baseWindow = new OpenTK.GameWindow(800, 600, new OpenTK.Graphics.GraphicsMode(32, 8, 0, 0));
 
             Game game = new Game(baseWindow, 3, 3, 2);
             baseWindow.Run();
